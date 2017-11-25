bf67ad4597d388dfb580dc24b24355fb5af62148
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-25 23:54:33 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916154312) do
+ActiveRecord::Schema.define(version: 20171125235035) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digets"
+    t.string "password_digest"
   end
 
 end
