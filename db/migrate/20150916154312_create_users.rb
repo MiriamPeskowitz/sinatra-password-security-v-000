2cdb2be1cf6fa28081e3f27979f4b8cd929f1032
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-25 23:35:51 +0000

    automatically backed up by learn

--- db/migrate/20150916154312_create_users.rb
+++ db/migrate/20150916154312_create_users.rb
@@ -3,10 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digets
-    end 
-  end 
+    end
+  end
 
-  def down 
+  def down
     drop_table :users
-  end 
+  end
 end 
